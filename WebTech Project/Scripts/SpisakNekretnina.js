ee3b269775e9aedb42cc7fda78c66fd7f0eac396
Benjamin Bandić <bbandic1@etf.unsa.ba>
2024-11-29 21:39:56 +0100

    Added criteria

--- WebTech Project/Scripts/SpisakNekretnina.js
+++ WebTech Project/Scripts/SpisakNekretnina.js
@@ -8,33 +8,42 @@ let SpisakNekretnina = function () {
         this.listaKorisnika = listaKorisnika;
     }
 
-    let filtrirajNekretnine = function (kriterij) {
+    let filtrirajNekretnine = function(kriterij) {
         return this.listaNekretnina.filter(nekretnina => {
-
             if (kriterij.tip_nekretnine && nekretnina.tip_nekretnine !== kriterij.tip_nekretnine) {
                 return false;
             }
-
+            if (kriterij.naziv && !nekretnina.naziv.toLowerCase().includes(kriterij.naziv.toLowerCase())) {
+                return false;
+            }
             if (kriterij.min_kvadratura && nekretnina.kvadratura < kriterij.min_kvadratura) {
                 return false;
             }
-
             if (kriterij.max_kvadratura && nekretnina.kvadratura > kriterij.max_kvadratura) {
                 return false;
             }
-
             if (kriterij.min_cijena && nekretnina.cijena < kriterij.min_cijena) {
                 return false;
             }
-
             if (kriterij.max_cijena && nekretnina.cijena > kriterij.max_cijena) {
                 return false;
             }
-
+            if (kriterij.tip_grijanja && nekretnina.tip_grijanja !== kriterij.tip_grijanja) {
+                return false;
+            }
+            if (kriterij.lokacija && !nekretnina.lokacija.toLowerCase().includes(kriterij.lokacija.toLowerCase())) {
+                return false;
+            }
+            if (kriterij.godina_izgradnje && nekretnina.godina_izgradnje !== kriterij.godina_izgradnje) {
+                return false;
+            }
+            if (kriterij.datum_objave && nekretnina.datum_objave !== kriterij.datum_objave) {
+                return false;
+            }
             return true;
         });
     }
-
+    
     let ucitajDetaljeNekretnine = function (id) {
         return listaNekretnina.find(nekretnina => nekretnina.id === id) || null;
     }
